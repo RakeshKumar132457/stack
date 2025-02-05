ded15a586f082738520ae6817eee25979ca7096d
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2025-02-05 14:21:44 +0530

    refactor: add beforeEach() in the test suite

--- src/stack.test.ts
+++ src/stack.test.ts
@@ -1,13 +1,16 @@
 import { Stack } from "./stack";
 
 describe("Stack", () => {
+  let stack: Stack;
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
   it("should return true for a new stack when checked if empty", () => {
-    let stack = new Stack();
     expect(stack.isEmpty()).toBe(true);
   });
 
   it("should return 0 when new stack size is checked", () => {
-    let stack = new Stack();
     expect(stack.size()).toBe(0);
   });
 });
