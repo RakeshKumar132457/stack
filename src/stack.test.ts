e564076f3c31105c3013d7e585f3ce1173e2fa1e
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2025-02-05 14:12:41 +0530

    test: add test to check the size of new stack

--- src/stack.test.ts
+++ src/stack.test.ts
@@ -5,4 +5,9 @@ describe("Stack", () => {
     let stack = new Stack();
     expect(stack.isEmpty()).toBe(true);
   });
+
+  it("should return 0 when new stack size is checked", () => {
+    let stack = new Stack();
+    expect(stack.size()).toBe(0);
+  });
 });
