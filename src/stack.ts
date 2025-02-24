ff6e5e54e7a16b35d0ff440a44df4b9154e102e5
Rakesh Kumar <rakesh.kumar.132457@gmail.com>
2025-02-24 22:00:56 +0530

    feat: implement push method for stack

--- src/stack.ts
+++ src/stack.ts
@@ -1,9 +1,19 @@
 export class Stack {
+  private items: Array<number>;
+  constructor() {
+    this.items = new Array<number>();
+  }
+
   isEmpty(): boolean {
     return true;
   }
 
   size(): number {
     return 0;
+  }
+
+  push(item: number): number {
+    this.items.push(item);
+    return item;
   }
 }
